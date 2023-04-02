063a66aa18a3efe295badcba16c940fdb415e107
Sam-rst <samuel.ressiot@gmail.com>
2023-04-02 23:44:05 +0200

    Blabla

--- 5_Requests/request2.sql
+++ 5_Requests/request2.sql
@@ -1,21 +1,18 @@
 -- ~~~~~ 2e Requête : Affichage 1~~~~~
 
 SELECT 
-Hebergement.*,  
-JSON_ARRAYAGG(EquipementExt.label),
-JSON_ARRAYAGG(EquipementInt.label), 
-JSON_ARRAYAGG(CategorieBien.label),
-JSON_ARRAYAGG(OptionHebergement.label), 
-JSON_ARRAYAGG(Service.label), 
-JSON_ARRAYAGG(ThemeSejour.label),  
-JSON_ARRAYAGG(Accessibilite.label),  
+Hebergement.*,
+DISTING JSON_ARRAYAGG(DEquipementExt.label),
+JSON_ARRAYAGG(EquipementInt.label),
+JSON_ARRAYAGG(OptionHebergement.label),
+JSON_ARRAYAGG(Service.label),
+JSON_ARRAYAGG(ThemeSejour.label),
 GROUP_CONCAT(Departement.code, Departement.nom SEPARATOR ' ,'),
-Hote.*,  
-JSON_ARRAYAGG(Photo.lien_cdn),  
-Chambre.*,  
-Avis.*,  
-GROUP_CONCAT(PeriodeSaison.nom, PeriodeSaison.tarif SEPARATOR ' ,'),
-JSON_ARRAYAGG(Classement.label)
+Hote.*,
+JSON_ARRAYAGG(Photo.lien_cdn),
+Chambre.*,
+Avis.*,
+Classement.label
 
 FROM Hebergement
 
@@ -26,10 +23,6 @@ LEFT JOIN EquipementExt ON EquipementExt_Hebergement.id_equipement_ext = Equipem
 LEFT JOIN EquipementInt_Hebergement ON Hebergement.id = EquipementInt_Hebergement.id_hebergement
 LEFT JOIN EquipementInt ON EquipementInt_Hebergement.id_equipement_int = EquipementInt.id
 
-LEFT JOIN CategorieBien_Hebergement ON Hebergement.id = CategorieBien_Hebergement.id_hebergement
-LEFT JOIN CategorieBien ON CategorieBien_Hebergement.id_categorie_bien = CategorieBien.id
-
-
 LEFT JOIN Hebergement_OptionHebergement ON Hebergement.id = Hebergement_OptionHebergement.id_hebergement
 LEFT JOIN OptionHebergement ON Hebergement_OptionHebergement.id_option = OptionHebergement.id
 
@@ -38,9 +31,6 @@ LEFT JOIN Service ON Hebergement_Service.id_service = Service.id
 
 LEFT JOIN Hebergement_ThemeSejour ON Hebergement.id = Hebergement_ThemeSejour.id_hebergement
 LEFT JOIN ThemeSejour ON Hebergement_ThemeSejour.id_theme_sejour = ThemeSejour.id
-
-LEFT JOIN Accessibilite_Hebergement ON Hebergement.id = Accessibilite_Hebergement.id_hebergement
-LEFT JOIN Accessibilite ON Accessibilite_Hebergement.id_accessible = Accessibilite.id
 
 LEFT JOIN Departement ON Hebergement.id_departement = Departement.id
 
@@ -51,8 +41,6 @@ LEFT JOIN Photo ON Hebergement.id = Photo.id_hebergement
 LEFT JOIN Chambre ON Hebergement.id = Chambre.id_hebergement
 
 LEFT JOIN Avis ON Hebergement.id = Avis.id_hebergement
-
-LEFT JOIN PeriodeSaison ON Hebergement.id = PeriodeSaison.id_hebergement
 
 LEFT JOIN Classement ON Hebergement.nombre_epis = Classement.nombre_epis
 
