c6b807634857ca90ae57687c5a3ca24845737012
Sam-rst <samuel.ressiot@gmail.com>
2023-04-04 13:06:43 +0200

    blabla

--- 5_Requests/request5.sql
+++ 5_Requests/request5.sql
@@ -5,5 +5,5 @@ SELECT * FROM Hebergement
 INNER JOIN Departement ON Hebergement.id_departement = Departement.id
 INNER JOIN Region ON Departement.id_region = Region.id
 INNER JOIN Disponibilite ON Hebergement.id = Disponibilite.id_hebergement
-WHERE Region.nom = 'Aquitaine' AND capacite = 8 AND Disponibilite.date_debut <= '2023-04-05' 
+WHERE Region.nom = 'Aquitaine' AND capacite = 11 AND Disponibilite.date_debut <= '2023-04-05' 
 AND Disponibilite.date_fin >= '2023-04-14' ;
